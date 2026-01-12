79aa49e2b65ca92de7185ce7a8c1731c120f7a0f
Wali-00 <waliu4574@gmail.com>
2026-01-12 12:20:40 +0600

    rating 281 resolve

--- 281.cpp
+++ 281.cpp
@@ -5,19 +5,23 @@ using namespace std;
 int main(){
     int T;
     cin>>T;
-    while (T--)
-    {
-        int X,Y;
-        cin>>X>>Y;
+   
+       
         while (T--)
         {
+             int X,Y;
+            cin>>X>>Y;
             if (X>Y)
             {
-                
+                cout<<"A"<<endl;
+            }
+            else
+            {
+                cout<<"B"<<endl;
             }
             
         }
         
-    }
+    
     
 }
