21f18d25ae9d0ac0bd080816addd9f2e1f7ce016
Wali-00 <waliu4574@gmail.com>
2026-01-12 12:26:19 +0600

    rating 281.1

---
+++ 281_.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin>>T;
+    while (T--)
+    {
+        int X,Y;
+        cin>>X>>Y;
+        if (X>Y)
+        {
+            cout<<X-Y<<endl;
+        }
+        
+    }
+    
+}
