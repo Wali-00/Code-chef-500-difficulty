cd90bc650b6b0287af2c04012e934795f32e0890
Wali-00 <waliu4574@gmail.com>
2026-01-13 18:56:12 +0600

    rating 285

---
+++ 285.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin>>T;
+    while (T--)
+    {
+        int X,H;
+        cin>>X>>H;
+        if (X<H)
+        {
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"Yes"<<endl;
+        }
+        
+    }
+    
+}
