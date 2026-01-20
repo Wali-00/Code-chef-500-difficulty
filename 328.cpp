b895861ad8335c1b68f12220252b9d121ad0f20a
Wali-00 <waliu4574@gmail.com>
2026-01-20 22:58:24 +0600

    rating 327 to 330

---
+++ 328.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin>>T;
+    while (T--)
+    {
+        int X;
+        cin>>X;
+        if (X<3)
+        {
+            cout<<"LIGHT"<<endl;
+
+        }
+        else if(X>=3 && X<7){
+            cout<<"MODERATE"<<endl;
+        }
+        else
+        {
+            cout<<"HEAVY"<<endl;
+        }
+        
+        
+    }
+    
+}
