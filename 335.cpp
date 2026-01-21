b037cf438ccb48b0e09abb88c4a2013bdbd2f597
Wali-00 <waliu4574@gmail.com>
2026-01-21 23:24:05 +0600

    rating 335 to 337

---
+++ 335.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin>>T;
+
+    while (T--)
+    {
+        int X;
+        cin>>X;
+        if(X<=70){
+            cout<<0<<endl;
+        }
+        else if (X>70 && X<=100)
+        {
+            cout<<500<<endl;
+        }
+        else{
+            cout<<2000<<endl;
+        }
+        
+    }
+    
+}
---
+++ 337.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin>>T;
+    while (T--)
+    {
+        int N;
+        cin>>N;
+        cout<<(N*60)/20<<endl;
+    }
+    
+}
