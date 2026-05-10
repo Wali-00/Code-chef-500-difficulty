d8aecdb5d385a93c488e799dd0d3d082ad67ac0f
Wali-00 <waliu4574@gmail.com>
2026-05-11 00:05:49 +0600

    350

---
+++ 350.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int T,X,Y,Z;
+    cin>>T;
+    while (T--)
+    {
+        cin>>X>>Y>>Z;
+        cout<<X*Y*Z<<endl;
+    }
+    
+}
