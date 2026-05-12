3b1d2f37a2e3e6447fec8dc276965a13c4e29fe1
Wali-00 <waliu4574@gmail.com>
2026-05-12 22:16:06 +0600

    problem rating 376

---
+++ 376.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T,X,Y;
+	cin>>T;
+	while(T--){
+	    cin>>X>>Y;
+	    
+	    if((X*3)<=Y){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
