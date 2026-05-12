5c0c29c731232ea285f0cc58560123eb0a82ebf4
Wali-00 <waliu4574@gmail.com>
2026-05-12 21:56:13 +0600

    problem rating 364

---
+++ 364.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T,X,Y;
+    cin>>T;
+    while(T--){
+        cin>>X>>Y;
+        if(X>Y){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+    }
+}
+}
