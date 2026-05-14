89b6259e2a4caa9b6f99e738f38393b3f9bfe2c3
Wali-00 <waliu4574@gmail.com>
2026-05-14 15:24:13 +0600

    rating 390.00

---
+++ 390__.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X,N,M;
+    cin>>X>>N>>M;
+    if(X+M>=N){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+
+}
