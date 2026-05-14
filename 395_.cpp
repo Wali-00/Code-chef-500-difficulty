b28d518e17fd1d10284aca4e21cbfed7bd09c2f6
Wali-00 <waliu4574@gmail.com>
2026-05-14 15:40:04 +0600

    rating 395.0

---
+++ 395_.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    if(X<=3){
+	        cout<<"BRONZE"<<endl;
+	}
+	else if(X>3 && X<=6){
+	    cout<<"SILVER"<<endl;
+	}
+	else{
+	    cout<<"GOLD"<<endl;
+	}
+	    
+	}
+
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    if(X<=3){
+	        cout<<"BRONZE"<<endl;
+	}
+	else if(X>3 && X<=6){
+	    cout<<"SILVER"<<endl;
+	}
+	else{
+	    cout<<"GOLD"<<endl;
+	}
+	    
+	}
+
+}
