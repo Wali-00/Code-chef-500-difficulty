11358f3cbef50fa23d77b0dcdebefe711f97892d
Wali-00 <waliu4574@gmail.com>
2026-05-17 22:44:06 +0600

    rating 419.0

---
+++ 419_.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int X;
+        double N;
+        cin>>N>>X;
+        if(N/2.0<=X){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+}
