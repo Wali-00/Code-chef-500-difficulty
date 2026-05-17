0f9acb1a0edd301d5b2c586f2d4af823b66299d3
Wali-00 <waliu4574@gmail.com>
2026-05-17 23:35:51 +0600

    rating 459

---
+++ 459.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X;
+    cin>>X;
+    if(X>=750){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+
