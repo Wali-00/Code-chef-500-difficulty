8dcbeeb95f26495f6fe2b878aa15ee3cf34a45ec
Wali-00 <waliu4574@gmail.com>
2026-05-17 22:58:25 +0600

    rating 434

---
+++ 434.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int X,Y,Z;
+        cin>>X>>Y>>Z;
+        if(X>Y && X>Z){
+            cout<<"Setter"<<endl;
+        }
+        else if(Y>X && Y>Z){
+            cout<<"Tester"<<endl;
+        }
+        else{
+            cout<<"Editorialist"<<endl;
+        }
+    }
+}
