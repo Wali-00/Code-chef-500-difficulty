8e478ccdaacccaafd3cba37992201b611bda99af
Wali-00 <waliu4574@gmail.com>
2026-05-18 21:05:16 +0600

    rating 475

---
+++ 475.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int X,Y;
+        cin>>X>>Y;
+        if(X>=Y){
+            cout<<Y<<endl;
+        }
+        else{
+            cout<<X<<endl;
+        }
+    }
+}
