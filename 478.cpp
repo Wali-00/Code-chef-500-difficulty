23adfee26bd17f8233e2e62268456470de9ca0be
Wali-00 <waliu4574@gmail.com>
2026-05-18 21:15:28 +0600

    rating 478

---
+++ 478.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int X;
+    cin>>X;
+    if(X+7>170){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
