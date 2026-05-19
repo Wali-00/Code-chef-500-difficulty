e87659c51782c3d5a91ab2216b5c0922627a3824
Wali-00 <waliu4574@gmail.com>
2026-05-19 22:33:47 +0600

    rating 489

---
+++ 489.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int X,N;
+        cin>>N>>X;
+        cout<<N/(X*3)<<endl;
+    }
+}
