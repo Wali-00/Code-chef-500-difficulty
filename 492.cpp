f70b0891c4bf125da7a88cb8ed0f998710d66c61
Wali-00 <waliu4574@gmail.com>
2026-05-19 22:38:22 +0600

    rating 492

---
+++ 492.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int C,X,Y;
+        cin>>C>>X>>Y;
+        cout<<(C-X)*Y<<endl;
+        
+    }
+}
